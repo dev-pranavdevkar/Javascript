04efccf12c93471a07781765920afe9bcfc05a50
Pranav Devkar <87268117+dev-pranavdevkar@users.noreply.github.com>
2024-07-18 17:23:46 +0000

    Setup of Git Codespace with required framework

---
+++ 01_basics/test.js
@@ -0,0 +1,13 @@
+console.log('Pranav Devkar');
+/* 
+1.Click on Three lines
+2.go to view
+3.click on command plate
+4.type >container and select add new Dev Config
+5.Serach Node /any Framework you want to configure
+6.Select needed configration of searched framework
+7 select Ok 
+8.then it asks for Click on rebuild
+9.Now you can use that framework
+
+*/
