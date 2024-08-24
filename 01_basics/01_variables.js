f93e4ad346bd01545c76c56fbe497b891210b169
Pranav Devkar <87268117+dev-pranavdevkar@users.noreply.github.com>
2024-08-24 14:46:43 +0000

    Datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,4 +4,4 @@ let email="devpranavdevkar@gmail.com";
 address="Kurlap";
 var pincode
 console.log(name,mobile,email,address,pincode);
-console.table([name,mobile,email,address,pincode]);
+console.table([name,mobile,email,address,pincode]); //To print in tabuler format on terminal
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+ num=24;
+ str="Pranav Devkar";
+ bool=true;
+ let undefined_value;
+ null_value=null
+console.table([[typeof num, num],[typeof str, str],[typeof bool, bool],[typeof undefined_value, undefined_value],[typeof null_value, null_value]]);
